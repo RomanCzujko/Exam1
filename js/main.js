adc74cc294d0a82bf44f335d7abebb50f99830cf
RomanCzujko <roman.chujko@gmail.com>
2020-07-14 16:58:00 +0300

    vers9

--- js/main.js
+++ js/main.js
@@ -66,20 +66,35 @@ $(function () {
   });
   //   $(".main-slider").appendDots();
 });
-//   $(".main-slider").slick({
-//     infinite: true,
-//     slidesToShow: 3,
-//     speed: 2000,
-//     autoplay: true,
-//     autoplaySpeed: 2000,
-//     // responsive: [
-//     //   {
-//     //     breakpoint: 768,
-//     //     settings: {
-//     //       slidesToShow: 3,
-//     //       dots: false,
-//     //     },
-//     //   },
-//     // ],
-//   });
-//
+
+// ------------------- Отправка формы
+
+$(document).ready(function () {
+  $("#form").submit(function () {
+    $.ajax({
+      type: "POST",
+      url: "send.php",
+      data: $(this).serialize(),
+    }).done(function () {
+      $(".js-overlay-thank-you").fadeIn();
+      $(this).find("input").val("");
+      $("#form").trigger("reset");
+    });
+    return false;
+  });
+});
+
+/// -----------Закрыть попап «спасибо»
+
+$(".js-close-thank-you").click(function () {
+  // по клику на крестик
+  $(".js-overlay-thank-you").fadeOut();
+});
+
+$(document).mouseup(function (e) {
+  // по клику вне попапа
+  var popup = $(".popup");
+  if (e.target != popup[0] && popup.has(e.target).length === 0) {
+    $(".js-overlay-thank-you").fadeOut();
+  }
+});
---
+++ send.php
@@ -0,0 +1,20 @@
+
+<?php
+
+$recepient = "roman.czujko@gmail.com";
+$siteName = "WebUpSteps - web-up-steps.com";
+
+$name = trim($_POST["name"]);
+$mail = trim($_POST["mail"]);
+$subject = trim($_POST["subject"]);
+$message = trim($_POST["message"]);
+
+$message = "Имя клиента: $name \nE-mail клиента: $mail\nСообщение: $message";
+
+$pagetitle = "Заявка с сайта \"$siteName\" ";
+
+mail($recepient, $pagetitle, $message, "Content-type: text/plain; charset=\"utf-8\"\n From: $recepient");
+
+
+?>
+
