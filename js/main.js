bbb348957a44623c49b4ca924abb6e1bd0194985
RomanCzujko <roman.chujko@gmail.com>
2020-08-06 04:02:26 +0300

    v25

--- js/main.js
+++ js/main.js
@@ -1,55 +1,68 @@
-$(document).on("scroll", window, function () {
+$(document).on('scroll', window, function () {
   if ($(window).scrollTop() < 400) {
-    $(".main-nav").hide();
+    $('.main-nav').hide()
   } else {
-    $(".main-nav").slideDown(700);
+    $('.main-nav').slideDown(700)
   }
-});
-$(".btn-cookies").on("click", function () {
-  $(".cookies").hide("cookies");
-});
+})
+$('.btn-cookies').on('click', function () {
+  $('.cookies').hide('cookies')
+})
 
 // ------------ Медленные якоря-------------
 $(document).ready(function () {
-  $("#slowmenu").on("click", "a", function (event) {
-    event.preventDefault();
-    let id = $(this).attr("href"),
-      top = $(id).offset().top;
-    $("body,html").animate({ scrollTop: top }, 1500);
-  });
-});
+  $('#slowmenu').on('click', 'a', function (event) {
+    event.preventDefault()
+    let id = $(this).attr('href'),
+      top = $(id).offset().top
+    $('body,html').animate({ scrollTop: top }, 1500)
+  })
+})
 $(document).ready(function () {
-  $("#slowmenu2").on("click", "a", function (event) {
-    event.preventDefault();
-    let id = $(this).attr("href"),
-      top = $(id).offset().top;
-    $("body,html").animate({ scrollTop: top }, 1500);
-  });
-});
+  $('#slowmenu2').on('click', 'a', function (event) {
+    event.preventDefault()
+    let id = $(this).attr('href'),
+      top = $(id).offset().top
+    $('body,html').animate({ scrollTop: top }, 1500)
+  })
+})
+// $('#1 a').click(function (e) {
+//   e.preventDefault()
+//   $(this).tab('show')
+// })
+// $('#2 a').click(function (e) {
+//   e.preventDefault()
+//   $(this).tab('show')
+// })
+// $('#3 a').click(function (e) {
+//   e.preventDefault()
+//   $(this).tab('show')
+// })
+// $(document).ready(function () {
+//   $('#slowmenu3').on('click', 'a', function (event) {
+//     event.preventDefault()
+//     let id = $(this).attr('href'),
+//       top = $(id).offset().top
+//     $('body,html').animate({ scrollTop: top }, 1500)
+//   })
+// })
 $(document).ready(function () {
-  $("#slowmenu3").on("click", "a", function (event) {
-    event.preventDefault();
-    let id = $(this).attr("href"),
-      top = $(id).offset().top;
-    $("body,html").animate({ scrollTop: top }, 1500);
-  });
-});
-$(document).ready(function () {
-  $("#anchor").on("click", "a", function (event) {
-    event.preventDefault();
-    let id = $(this).attr("href"),
-      top = $(id).offset().top;
-    $("body,html").animate({ scrollTop: top }, 1500);
-  });
-});
+  $('#anchor').on('click', 'a', function (event) {
+    event.preventDefault()
+    let id = $(this).attr('href'),
+      top = $(id).offset().top
+    $('body,html').animate({ scrollTop: top }, 1500)
+  })
+})
 // ------------------- SLIDER --------------
 $(function () {
-  $(".main-slider").slick({
+  $('.main-slider-websites').slick({
     infinite: true,
     slidesToShow: 1,
     speed: 2000,
     autoplay: true,
     autoplaySpeed: 2000,
+    cssEase: 'linear',
     responsive: [
       {
         breakpoint: 768,
@@ -60,41 +73,41 @@ $(function () {
       },
     ],
     arrows: true,
-    appendArrows: $(".your-class-arrow"),
+    appendArrows: $('.your-class-arrow'),
     prevArrow: '<i class="fa fa-chevron-left" aria-hidden="true"></i> ',
     nextArrow: '<i class="fa fa-chevron-right" aria-hidden="true"></i>',
-  });
+  })
   //   $(".main-slider").appendDots();
-});
+})
 
 // ------------------- Отправка формы
 
 $(document).ready(function () {
-  $("#form").submit(function () {
+  $('#form').submit(function () {
     $.ajax({
-      type: "POST",
-      url: "send.php",
+      type: 'POST',
+      url: 'send.php',
       data: $(this).serialize(),
     }).done(function () {
-      $(".js-overlay-thank-you").fadeIn();
-      $(this).find("input").val("");
-      $("#form").trigger("reset");
-    });
-    return false;
-  });
-});
+      $('.js-overlay-thank-you').fadeIn()
+      $(this).find('input').val('')
+      $('#form').trigger('reset')
+    })
+    return false
+  })
+})
 
 /// -----------Закрыть попап «спасибо»
 
-$(".js-close-thank-you").click(function () {
+$('.js-close-thank-you').click(function () {
   // по клику на крестик
-  $(".js-overlay-thank-you").fadeOut();
-});
+  $('.js-overlay-thank-you').fadeOut()
+})
 
 $(document).mouseup(function (e) {
   // по клику вне попапа
-  var popup = $(".popup");
+  var popup = $('.popup')
   if (e.target != popup[0] && popup.has(e.target).length === 0) {
-    $(".js-overlay-thank-you").fadeOut();
+    $('.js-overlay-thank-you').fadeOut()
   }
-});
+})
