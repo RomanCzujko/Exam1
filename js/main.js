a9b0fe4bfe2c47de169de8c959127b750e5d06d8
RomanCzujko <roman.chujko@gmail.com>
2020-09-21 17:43:56 +0300

    4.0

--- js/main.js
+++ js/main.js
@@ -26,7 +26,22 @@ $(document).ready(function () {
     $('body,html').animate({ scrollTop: top }, 1500)
   })
 })
-
+$(document).ready(function () {
+  $('#landing').on('click', 'a', function (event) {
+    event.preventDefault()
+    let id = $(this).attr('href'),
+      top = $(id).offset().top
+    $('body,html').animate({ scrollTop: top }, 1500)
+  })
+})
+$(document).ready(function () {
+  $('#shop').on('click', 'button', function (event) {
+    event.preventDefault()
+    let id = $(this).attr('href'),
+      top = $(id).offset().top
+    $('body,html').animate({ scrollTop: top }, 1500)
+  })
+})
 // $('#1 a').click(function (e) {
 //   e.preventDefault()
 //   $(this).tab('show')
